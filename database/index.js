146462180b943098c25fe3dfd2020d9088f7a713
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-16 11:59:41 -0300

    added resgiter

--- database/index.js
+++ database/index.js
@@ -1,24 +1,50 @@
-import { resolve } from 'path';
-import { readFile } from 'fs';
-import { promisify } from 'util';
+import { resolve } from 'path'
+import { readFile, writeFile } from 'fs'
+import { promisify } from 'util'
 
-const readFileAsync = promisify(readFile);
+const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
 
 class Database {
   constructor() {
-    this.NAME_FILE = 'heroes.json';
+    this.NAME_FILE = 'heroes.json'
   }
 
   async getDatasFiles() {
-    const file = await readFileAsync(resolve('./database', this.NAME_FILE), { encoding: 'utf-8' });
-    return JSON.parse(file.toString());
+    const file = await readFileAsync(resolve('./database', this.NAME_FILE), {
+      encoding: 'utf-8',
+    })
+    return JSON.parse(file.toString())
+  }
+
+  async writeFiles(data) {
+    await writeFileAsync(
+      resolve('./database', this.NAME_FILE),
+      JSON.stringify(data)
+    )
+    return true
+  }
+
+  async register(hero) {
+    const data = await this.getDatasFiles()
+    const id = hero.id <= 2 ? hero.id : Date.now()
+    const heroWithId = {
+      ...hero,
+      id,
+    }
+
+    const newData = [...data, heroWithId]
+
+    const result = await this.writeFiles(newData)
+
+    return result
   }
 
   async list(id) {
-    const data = await this.getDatasFiles();
-    const dataFilter = data.filter((item) => (id ? item.id === id : true));
-    return dataFilter;
+    const data = await this.getDatasFiles()
+    const dataFilter = data.filter((item) => (id ? item.id === id : true))
+    return dataFilter
   }
 }
 
-export default new Database();
+export default new Database()
