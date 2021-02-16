1d086625fdf1e1196593ad6e8393171927304deb
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-16 12:00:04 -0300

    added main

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/extensions */
-import dataBase from '../database/index.js';
-
-async function main() {
-  const data = await dataBase.getDatasFiles();
-  console.log(data);
-}
-
-main();
