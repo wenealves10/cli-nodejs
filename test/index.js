82072c3894c242f4376ffb44dfee3edb252a4059
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-17 16:56:12 -0300

    new tests

--- test/index.js
+++ test/index.js
@@ -8,9 +8,16 @@ const DEFAULT_ITEM = {
   id: 2,
 }
 
+const DEFAULT_ITEM_UPDATE = {
+  name: 'Batman',
+  power: 'Money',
+  id: 1,
+}
+
 describe('Registration manipulation suite', () => {
   before(async () => {
     await dataBase.register(DEFAULT_ITEM)
+    await dataBase.register(DEFAULT_ITEM_UPDATE)
   })
 
   it('Must list heroes by id correctly', async () => {
@@ -30,5 +37,32 @@ describe('Registration manipulation suite', () => {
     const [actual] = await dataBase.list(DEFAULT_ITEM.id)
 
     deepStrictEqual(actual, expected)
+  })
+
+  it('must remove a hero by id', async () => {
+    const expected = true
+
+    const result = await dataBase.remove(DEFAULT_ITEM.id)
+
+    deepStrictEqual(result, expected)
+  })
+
+  it('must update hero by id', async () => {
+    const expected = {
+      ...DEFAULT_ITEM_UPDATE,
+      name: 'Superman',
+      power: 'Super strength',
+    }
+
+    const newItem = {
+      name: 'Superman',
+      power: 'Super strength',
+    }
+
+    await dataBase.update(DEFAULT_ITEM_UPDATE.id, newItem)
+
+    const [result] = await dataBase.list(DEFAULT_ITEM_UPDATE.id)
+
+    deepStrictEqual(result, expected)
   })
 })
